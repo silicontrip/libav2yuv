ff2e6e5a52159dc80b0f4fcabafc8c62a88a7fb3
Mark Heath <mark@silicontrip.net>
2012-10-31 08:02:08 +1100

    Cleaning up code in preparation for audio decoding.

--- test1.m
+++ test1.m
@@ -11,22 +11,100 @@
 
 NSAutoreleasePool  *pool;
 
+void processVideo (libav2yuvArguments *options, NSArray *edlList)
+{
+	
+	AVObject *lav = [edlList objectAtIndex:0];
+	
+	[lav dumpFormat];
+	
+	NSLog(@"%dx%d by %d:%d at %d:%d",[lav getWidth],[lav getHeight],[lav getSampleAspectNum],[lav getSampleAspectDen],[lav getFrameRateNum],[lav getFrameRateDen]);
+	
+	libyuv *yuv;
+	
+	// NSLog(@"libyuv alloc");
+	yuv = [[libyuv alloc] 
+		   initWithWidth:[lav getWidth] 
+		   Height:[lav getHeight] 
+		   SampleAspectAVRational:[lav getSampleAspect]
+		   FrameRateAVRational:[lav getFrameRate]
+		   ChromaFromAV:[lav getChromaSampling]];
+	
+	if ([options getExtensions])
+	{
+		[yuv setExtensions:1];
+	}
+	if ([options hasAspect]) {
+		[yuv setSampleAspect:[options getAspect]];
+	}
+	if ([options hasFrameRate]) {
+		[yuv setFrameRate:[options getFrameRate]];
+	}
+	if ([options hasChroma]){
+		[yuv setChromaSampling:[options getChroma]];
+	}		
+	if ([options hasOutFile]){
+		[yuv setOutputFilename:[options getOutFile]];
+	}
+	// need to decode the first frame to get the interlace type
+	// NSLog(@"lav decodeNextFrame");
+	
+	[lav decodeNextFrame];
+	// NSLog(@"yuv setYUVFrameDataWithAVFrame");
+	
+	[yuv setYUVFrameDataWithAVFrame:[lav getAVFrame]];
+	// NSLog(@"yuv setInterlaceAndOrder");
+	
+	[yuv setInterlaceAndOrder:[lav getIsInterlaced] topFieldFirst:[lav getInterlaceTopFieldFirst]];
+	//interlace flag is not available until the first frame is decoded.
+	//need to get the interlace flags before this. So we can use a generator.
+	// NSLog(@"yuv writeHeader");
+	
+	[yuv writeHeader];
+	[yuv write];
+	// [lav release];
+	
+	for (AVObject *video in edlList) {
+		if (video != nil) {
+			
+			while ([video decodeNextFrame] >=0)
+			{
+				[yuv setYUVFrameDataWithAVFrame:[video getAVFrame]];
+				[yuv write];
+			}
+			[video release];
+		} else {
+			NSLog(@"Couldn't initialise video");
+		}
+		
+	}
+	[yuv release];
+	
+	
+}
+
 int main(int argc, char *argv[])
 {
 	
 	NSMutableArray *edlList;
 	pool = [[NSAutoreleasePool alloc] init];
 	edlList = [NSMutableArray arrayWithCapacity:1];
+	int streamMode;
 	
 	libav2yuvArguments *options = [[libav2yuvArguments alloc]  initWithNSProcessInfoArguments:[[NSProcessInfo processInfo] arguments]];
 	
+	if ([options getAudio] ) {
+		streamMode = AVMEDIA_TYPE_AUDIO;
+	} else {
+		streamMode = AVMEDIA_TYPE_VIDEO;
+	}
 	
 	for (NSString *argument in [options getArguments]) {
 		AVObject *lav = nil;
 		if ([argument hasSuffix:@".edl"]) {
-			lav = [[edlListFilter alloc] initWithFile:argument];
+			lav = [[edlListFilter alloc] initWithFile:argument streamType:streamMode];
 		} else  {	
-			lav = [[libav alloc] initVideoWithFile:argument];
+			lav = [[libav alloc] initWithFile:argument streamType:streamMode stream:-1];
 		}
 		if (lav != nil) 
 		{
@@ -45,76 +123,48 @@ int main(int argc, char *argv[])
 	
 	if ([edlList count] > 0 ) 
 	{
-		AVObject *lav = [edlList objectAtIndex:0];
-		
-		[lav dumpFormat];
-		
-		NSLog(@"%dx%d by %d:%d at %d:%d",[lav getWidth],[lav getHeight],[lav getSampleAspectNum],[lav getSampleAspectDen],[lav getFrameRateNum],[lav getFrameRateDen]);
-		
-		libyuv *yuv;
-		
-		// NSLog(@"libyuv alloc");
-		yuv = [[libyuv alloc] 
-			   initWithWidth:[lav getWidth] 
-			   Height:[lav getHeight] 
-			   SampleAspectAVRational:[lav getSampleAspect]
-			   FrameRateAVRational:[lav getFrameRate]
-			   ChromaFromAV:[lav getChromaSampling]];
-		
-		if ([options getExtensions])
-		{
-			[yuv setExtensions:1];
-		}
-		if ([options hasAspect]) {
-			[yuv setSampleAspect:[options getAspect]];
-		}
-		if ([options hasFrameRate]) {
-			[yuv setFrameRate:[options getFrameRate]];
-		}
-		if ([options hasChroma]){
-			[yuv setChromaSampling:[options getChroma]];
-		}		
-		if ([options hasOutFile]){
-			[yuv setOutputFilename:[options getOutFile]];
-		}
-		// need to decode the first frame to get the interlace type
-		NSLog(@"lav decodeNextFrame");
-		
-		[lav decodeNextFrame];
-		NSLog(@"yuv setYUVFrameDataWithAVFrame");
-		
-		[yuv setYUVFrameDataWithAVFrame:[lav getAVFrame]];
-		NSLog(@"yuv setInterlaceAndOrder");
 		
 		
-		
-		[yuv setInterlaceAndOrder:[lav getIsInterlaced] topFieldFirst:[lav getInterlaceTopFieldFirst]];
-		//interlace flag is not available until the first frame is decoded.
-		//need to get the interlace flags before this. So we can use a generator.
-		// NSLog(@"yuv writeHeader");
-		
-		[yuv writeHeader];
-		[yuv write];
-		// [lav release];
-		
-		for (AVObject *video in edlList) {
-			if (video != nil) {
-				
-				
-				while ([video decodeNextFrame] >=0)
-				{
-					[yuv setYUVFrameDataWithAVFrame:[video getAVFrame]];
-					[yuv write];
-					
-				}
-				
-				[video release];
+		if (streamMode == AVMEDIA_TYPE_VIDEO) {
+			processVideo(options,edlList);
+		} else {
+			
+			NSFileHandle *nfh;
+			if ([options hasOutFile]){
+				nfh = [NSFileHandle fileHandleForWritingAtPath:[options getOutFile]];
 			} else {
-				NSLog(@"Couldn't initialise video %s",argv[1]);
+				nfh = [NSFileHandle fileHandleWithStandardOutput];
 			}
 			
+			// decode audio
+			
+			AVObject *lav = [edlList objectAtIndex:0];
+			
+			[lav dumpFormat];
+			
+			// open out file
+			
+			for (AVObject *audio in edlList) {
+				if (audio != nil) {
+					while ([audio decodeNextAudio] >=0)
+					{
+					
+						AVFrame *pFrame = [audio getAVFrame];
+					
+						NSData *data = [NSData dataWithBytes:pFrame->data[0] length:pFrame->nb_samples];
+					
+						[nfh writeData:data];
+					}
+					[audio release];
+				} else {
+					NSLog(@"Couldn't initialise video %s",argv[1]);
+				}
+				
+			}
+			// close file
+			[nfh closeFile];
+			
 		}
-		[yuv release];
 	}
 	[pool release];
 	
