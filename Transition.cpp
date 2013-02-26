9474d82dd75520d75e69bbb535ca94bbd18a7ab2
Mark Heath (WXB035881PR830) <mark@silicontrip.org>
2013-02-26 14:24:03 +1100

    New Transition base class

---
+++ Transition.cpp
@@ -0,0 +1,49 @@
+#include "Transition.h"
+
+
+Transition::Transition(AVObject *f, AVObject *t, unsigned int fr)
+{
+	this->setFrames(fr);
+	
+	this->setAVFrom(f);
+	this->setAVTo(t);
+		
+	frameCounter = 0;
+	
+	this->allocFrame();
+}
+
+
+Transition::~Transition(void)
+{
+	delete fromAV;
+	delete toAV;
+}
+
+void DissolveTransition::setAVFrom (AVObject *s)
+{
+	fromAV = s;
+	
+	this->setFrameRate(s->getFrameRate());
+	this->setSampleAspect(s->getSampleAspect());
+	this->setHeight(s->getHeight());
+	this->setWidth(s->getWidth());
+	this->setChromaSampling(s->getChromaSampling());
+
+	// we don't get interlace information until the first frame is decoded.
+	
+	/*
+	 this->setInterlaced(s->getIsInterlaced());
+	 this->setInterlaceTopFieldFirst(s->getInterlaceTopFieldFirst());
+	 */
+}	
+
+void DissolveTransition::setAVTo (AVObject *s)
+{
+	toAV = s;
+}	
+
+void DissolveTransition::setFrames(unsigned int f)
+{
+	frames = f;
+}
---
+++ Transition.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "AVobject.h"
+class Transition :
+	public AVObject
+{
+protected:
+	AVObject * fromAV;
+	AVObject * toAV;
+
+	int frameCounter;
+	
+	unsigned int frames;
+public:
+	Transition(AVObject *f, AVObject *t, unsigned int fr);
+	~Transition(void);
+
+	void setAVFrom (AVObject *);
+	void setAVTo (AVObject *);
+	void setFrames (unsigned int);
+	
+	abstract int decodeNextFrame(void);
+	abstract void dumpFormat(void);
+};
+
